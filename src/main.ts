a4fb6d255e338cc3e6fc02fc9c77640ea5398447
Devon <devon.blackbeard@gmail.com>
2024-05-19 18:08:29 -0600

    Create medium clone with angular and ngrx

---
+++ src/main.ts
@@ -0,0 +1,15 @@
+import {bootstrapApplication} from '@angular/platform-browser'
+import {provideRouter} from '@angular/router'
+import {AppComponent} from './app/app.component'
+import {appRoutes} from './app/app.routes'
+import { provideStore } from '@ngrx/store'
+import { provideStoreDevtools } from '@ngrx/store-devtools'
+import { isDevMode } from '@angular/core'
+
+bootstrapApplication(AppComponent, {providers: [provideRouter(appRoutes), provideStore(), provideStoreDevtools({
+    maxAge: 25,
+    logOnly: !isDevMode(),
+    autoPause: true,
+    trace: false,
+    traceLimit: 75
+})]})
