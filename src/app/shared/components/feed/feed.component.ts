f64ba49ff1b7d4f5e8453788076dd3fee3d41c3f
Devon <devon.blackbeard@gmail.com>
2024-10-14 19:15:06 -0600

    More pagination work

--- src/app/shared/components/feed/feed.component.ts
+++ src/app/shared/components/feed/feed.component.ts
@@ -1,18 +1,26 @@
 import {CommonModule} from '@angular/common'
 import {Component, Input, OnInit} from '@angular/core'
-import {RouterLink} from '@angular/router'
+import {ActivatedRoute, Params, Router, RouterLink} from '@angular/router'
 import {Store} from '@ngrx/store'
 import {combineLatest} from 'rxjs'
 import {feedActions} from './store/actions'
 import {selectError, selectFeedData, selectIsLoading} from './store/reducers'
-import { ErrorMessageComponent } from "../errorMessage/errorMessage.component";
-import { LoadingComponent } from "../loading/loading.component";
+import {ErrorMessageComponent} from '../errorMessage/errorMessage.component'
+import {LoadingComponent} from '../loading/loading.component'
+import {environment} from 'src/environments/environment.development'
+import {PaginationComponent} from '../pagination/pagination.component'
 
 @Component({
   selector: 'mc-feed',
   templateUrl: './feed.component.html',
   standalone: true,
-  imports: [CommonModule, RouterLink, ErrorMessageComponent, LoadingComponent],
+  imports: [
+    CommonModule,
+    RouterLink,
+    ErrorMessageComponent,
+    LoadingComponent,
+    PaginationComponent,
+  ],
 })
 export class FeedComponent implements OnInit {
   @Input() apiUrl: string = ''
@@ -22,10 +30,23 @@ export class FeedComponent implements OnInit {
     error: this.store.select(selectError),
     feed: this.store.select(selectFeedData),
   })
+  limit = environment.limit
+  baseUrl = this.router.url.split('?')[0]
+  currentPage: number = 0
 
-  constructor(private store: Store) {}
+  constructor(
+    private store: Store,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
     this.store.dispatch(feedActions.getFeed({url: this.apiUrl}))
+    this.route.queryParams.subscribe((params: Params) => {
+      // {page: '1'}
+      console.log(params);
+      
+      this.currentPage = Number(params['page'] || '1')
+    })
   }
 }
